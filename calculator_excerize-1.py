af95aa41d4195af4a2eb63ad20899d7cbad19859
PiyushBorseGit <144165717+PiyushBorseGit@users.noreply.github.com>
2023-09-08 01:17:01 +0530

    Add files via upload

---
+++ calculator_excerize-1.py
@@ -0,0 +1,9 @@
+a = input("enyer first num :")
+b = input("enyer second num :")
+
+print(int(a) + int(b))
+print(int(a) - int(b))
+print(int(a) * int(b))
+print(int(a) / int(b))
+print(int(a) % int(b))
+print(int(a) // int(b))
